fd1394228f0cc3500831aa7aec115102968c4f09
Karim Bakkali <kbldn@live.co.uk>
2020-01-14 07:28:39 +0000

    Initial Commit
    adding the first commit with the the scene and player build

---
+++ Assets/Scripts/AddPlayerControlledVelocity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Movement;
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Movement;
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Movement;
+    }
+}
